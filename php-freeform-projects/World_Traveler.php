0521c8565db97749ba080440f5aac43473c954cb
AndrewCTer <33009070+AndrewTer@users.noreply.github.com>
2019-10-21 02:34:24 +0300

    Update World_Traveler.php

--- php-freeform-projects/World_Traveler.php
+++ php-freeform-projects/World_Traveler.php
@@ -1,1 +1,24 @@
+<?php
+  $riel = 2103942;
+  $kyat = 19092;
+  $krones = 109;
+  $lek = 9094;
 
+  echo "We have $riel riel, $kyat kyat, $krones krones, $lek lek";
+
+  // exchange rate on 21 oct 2019
+  $riel_exchange_rate = 0.00025;
+  $kyat_exchange_rate = 0.00065;
+  $krones_exchange_rate = 0.11;
+  $lek_exchange_rate = 0.0090;
+
+  $riel_to_usd = round($riel * $riel_exchange_rate, 2);
+  $kyat_to_usd = round($kyat * $kyat_exchange_rate, 2);
+  $krones_to_usd = round($krones * $krones_exchange_rate, 2);
+  $lek_to_usd = round($lek * $lek_exchange_rate, 2);
+
+  echo "\n\nThe amount of USD:\nriel = $riel_to_usd \nkyat = $kyat_to_usd \nkrones = $krones_to_usd \nlek = $lek_to_usd";
+  echo "\nFinal amount: ".round($riel_to_usd + $kyat_to_usd + $krones_to_usd + $lek_to_usd - 1, 2);
+  
+
+?>
