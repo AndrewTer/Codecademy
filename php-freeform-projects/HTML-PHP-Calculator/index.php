98af4f5a2d94904b7ecfc2ccd7ed6743cbf36133
AndrewCTer <33009070+AndrewTer@users.noreply.github.com>
2019-10-22 15:25:04 +0300

    Create index.php

---
+++ php-freeform-projects/HTML-PHP-Calculator/index.php
@@ -0,0 +1,19 @@
+<html>
+<body>
+  <p>Addition</p>
+  <form method="get" action="addition.php">
+    X: <input type="number" name="add_x"><br />
+    Y: <input type="number" name="add_y"><br />
+    <button type="submit">Calculate</button><br />
+  </form>
+  
+  <p>Division</p>
+  <form method="get" action="division.php">
+    X: <input type="number" name="div_x"><br />
+    Y: <input type="number" name="div_y"><br />
+    <button type="submit">Calculate</button><br />
+  </form>
+
+  <a href="index.php">Reset</a>
+</body>
+</html>
